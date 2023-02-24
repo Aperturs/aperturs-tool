e6a0a89f085b5b34e2094095c82651ca9f53cc43
swaraj bachu <rajswaraj.r@gmail.com>
2023-02-24 14:51:24 +0530

    vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-
-  return <Component {...pageProps} />  
-
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
- 
+
+export default MyApp;
